886ee2a455c6362bea4eb07e196584ee330a2f0f
Ubuntu Rendy <154489380+Rendzy55@users.noreply.github.com>
2026-04-24 01:15:57 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Legacy file dari versi WebAR lama.
-// Sekarang proyek menggunakan halaman non-AR:
-// index.html -> viewer.html -> viewer.js
-
-console.info('Legacy script.js loaded. Gunakan viewer.js untuk 3D viewer terbaru.');
